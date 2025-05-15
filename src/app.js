8323a451a28447df9cf61803002712aebc9a7614
michelle <mwang@Mac.monkeybrains.net>
2025-05-14 23:18:51 -0700

    add validation error and exception handling middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const routes = require('./routes/routes')
 const boardRoutes = require('./routes/boardRoutes.js')
 const cardRoutes = require('./routes/cardRoutes.js')
+const { ValidationError } = require('./exception/errors.js')
 
 const app = express()
 app.use(express.json())
@@ -13,5 +14,14 @@ app.use(cors())
 app.use('/', routes)
 app.use('/boards', boardRoutes)
 app.use('/card', cardRoutes)
+
+// Exception handling
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json({ error: err.message })
+  }
+
+  res.status(500).json({ error: 'Internal Server Error' })
+})
 
 module.exports = app
